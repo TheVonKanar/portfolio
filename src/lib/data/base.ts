df54f1dc5813d9a53852acb8f1dadf8d903cc86a
Thibault Eynard <thibault.eynard@hotmail.fr>
2025-05-26 19:26:25 +0200

    Update base data

--- src/lib/data/base.ts
+++ src/lib/data/base.ts
@@ -1,6 +1,6 @@
-const firstName = 'Riadh';
-const lastName = 'Adrani';
-const suffix = 'Slick Portfolio With Svelte 5';
+const firstName = 'Thibault';
+const lastName = 'Eynard';
+const suffix = 'Portfolio';
 
 const BaseData = {
 	firstName,
