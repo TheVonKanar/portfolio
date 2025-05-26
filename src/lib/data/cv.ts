735f57278c53cd2d9eca9776d3d8515362dda510
Thibault Eynard <thibault.eynard@hotmail.fr>
2025-05-26 19:26:56 +0200

    Update CV (previously called resume) page

---
+++ src/lib/data/cv.ts
@@ -0,0 +1,9 @@
+import { href } from '$lib/utils';
+
+const title = 'CV';
+
+const cv = href('/pdf/CV_ThibaultEynard.pdf');
+
+const CVData = { title, cv };
+
+export default CVData;
---
+++ src/routes/cv/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import TitledPage from '$lib/components/common/titled-page/titled-page.svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import CVData from '$lib/data/cv';
+</script>
+
+<TitledPage title={CVData.title}>
+	<a href={CVData.cv} class="mx-auto">
+		<Button>Download</Button>
+	</a>
+	<iframe src={CVData.cv} class="h-full w-full" title={CVData.title}></iframe>
+</TitledPage>
